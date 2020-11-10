2258fdb53868bdfbf93ddcfa3d61777ded057f13
Hasan Can <aslan4044@gmail.com>
2020-11-10 17:39:34 +0300

    test: add logger classes

---
+++ src/main/java/dmme/kuvid/Application.java
@@ -0,0 +1,47 @@
+package dmme.kuvid;
+
+import javax.swing.*;
+import java.io.IOException;
+
+public class Application implements Runnable {
+    private static volatile Application _instance = null;
+    private final Logger logger = LoggerFactory.getInstance().getLogger(getClass());
+
+    private Application() {
+        logger.i("Created new KUVid instance.");
+    }
+
+    public static synchronized Application getInstance() {
+        if (_instance == null) {
+            _instance = new Application();
+        }
+        return _instance;
+    }
+
+    public static void main(String[] args) {
+        Application application = Application.getInstance();
+        SwingUtilities.invokeLater(application);
+    }
+
+    @Override
+    public void run() {
+        logger.i("Running KUVid...");
+        startInitialization();
+    }
+
+    public void startInitialization() {
+
+    }
+
+    public boolean startClient(String clientName, String ip, int port) {
+
+    }
+
+    public boolean startServer(int port) {
+
+    }
+
+    public void startGame() {
+
+    }
+}
