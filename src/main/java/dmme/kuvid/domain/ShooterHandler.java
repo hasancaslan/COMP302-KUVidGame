2f0a1b0ce78906f120270a3cea0951faa3e38db4
Hasan Can <aslan4044@gmail.com>
2020-11-29 19:30:02 +0300

    feat: implement shooter handler domain model

---
+++ src/main/java/dmme/kuvid/domain/ShooterHandler.java
@@ -0,0 +1,17 @@
+package dmme.kuvid.domain;
+
+public class ShooterHandler {
+    private Shooter shooter;
+
+    public ShooterHandler(Shooter shooter) {
+        this.shooter = shooter;
+    }
+
+    public void moveShooter(double displacement) {
+
+    }
+
+    public void rotateShooter(int angle) {
+
+    }
+}
