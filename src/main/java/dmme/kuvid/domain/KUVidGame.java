5def57726fc4cc168ba4ded9a253ce47cfe7a027
Hasan Can <aslan4044@gmail.com>
2020-11-29 19:28:44 +0300

    feat: initial implementation of game controller

---
+++ src/main/java/dmme/kuvid/domain/KUVidGame.java
@@ -0,0 +1,59 @@
+package dmme.kuvid.domain;
+
+import java.sql.Time;
+
+public class KUVidGame {
+    public Time time;
+    public boolean active;
+    public boolean blendingMode;
+
+    public KUVidGame(Time time, boolean active, boolean blendingMode) {
+        this.time = time;
+        this.active = active;
+        this.blendingMode = blendingMode;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public boolean isBlendingMode() {
+        return blendingMode;
+    }
+
+    public void setBlendingMode(boolean blendingMode) {
+        this.blendingMode = blendingMode;
+    }
+
+    public void aimShooter(int angle) {
+
+    }
+
+    public void moveShooter(double displacement) {
+
+    }
+
+    public void selectAtom(AtomType type) {
+
+    }
+
+    public void selectPowerUp(PowerType type) {
+
+    }
+
+    public void useBlender(boolean mode, AtomType type, int number) {
+
+    }
+}
