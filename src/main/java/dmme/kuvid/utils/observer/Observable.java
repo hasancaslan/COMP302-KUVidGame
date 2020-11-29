11de0c5a9fd62fec597661aab26806d8ca3621b2
Hasan Can <aslan4044@gmail.com>
2020-11-29 19:26:32 +0300

    feat: implement observer classes for event listening

---
+++ src/main/java/dmme/kuvid/utils/observer/Observable.java
@@ -0,0 +1,63 @@
+package dmme.kuvid.utils.observer;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Observer for Observer pattern implementation. Allows PropertyListener observers.
+ *
+ * @see PropertyListener
+ * @see PropertyEvent
+ *
+ * @author DMMe
+ */
+public abstract class Observable {
+
+    protected transient Map<String, List<PropertyListener>> propertyListenersMap;
+
+    public Observable() {
+        initPropertyListeners();
+    }
+
+    public void addPropertyListener(PropertyListener listener) {
+        initPropertyListeners();
+        propertyListenersMap.get("all").add(listener);
+    }
+
+    public void addPropertyListener(String propertyName, PropertyListener listener) {
+        initPropertyListeners();
+        if (propertyListenersMap.containsKey(propertyName)) {
+            propertyListenersMap.get(propertyName).add(listener);
+        } else {
+            List<PropertyListener> list = new ArrayList<>();
+            list.add(listener);
+            propertyListenersMap.put(propertyName, list);
+        }
+    }
+
+    private void initPropertyListeners() {
+        if (propertyListenersMap == null) {
+            propertyListenersMap = new HashMap<String, List<PropertyListener>>();
+            propertyListenersMap.put("all", new ArrayList<PropertyListener>());
+        }else if(!propertyListenersMap.containsKey("all")) {
+            propertyListenersMap.put("all", new ArrayList<PropertyListener>());
+        }
+    }
+
+    public void publishPropertyEvent(String propertyName, Object oldValue, Object newValue) {
+        initPropertyListeners();
+
+        PropertyEvent pEvent = new PropertyEvent(this, propertyName, oldValue, newValue);
+        for (PropertyListener listener : propertyListenersMap.get("all")) {
+            listener.onPropertyEvent(pEvent);
+        }
+
+        if (propertyListenersMap.containsKey(propertyName)) {
+            for (PropertyListener listener : propertyListenersMap.get(propertyName)) {
+                listener.onPropertyEvent(pEvent);
+            }
+        }
+    }
+}
