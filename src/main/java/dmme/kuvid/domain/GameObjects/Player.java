fb8a19e5387f3c46fd14e132363569b07ea05d77
Hasan Can <aslan4044@gmail.com>
2020-12-10 01:48:17 +0300

    style: fix style of Player

--- src/main/java/dmme/kuvid/domain/GameObjects/Player.java
+++ src/main/java/dmme/kuvid/domain/GameObjects/Player.java
@@ -1,46 +1,43 @@
 package dmme.kuvid.domain.GameObjects;
 
-import dmme.kuvid.domain.KUVidGame;
-
 public class Player {
-	private int health=100;
-	private int point=0;
+    private static Player instance = null;
+    private int health = 100;
+    private int point = 0;
 
-	private static Player instance = null;
-	
-	private Player() {
-	}
+    private Player() {
+    }
 
-	public static Player getInstance() {
-		if (instance == null)
-			instance = new Player();
+    public static Player getInstance() {
+        if (instance == null)
+            instance = new Player();
 
-		return instance;
-	}
+        return instance;
+    }
 
-	public int getHealth() {
-		return health;
-	}
+    public int getHealth() {
+        return health;
+    }
 
-	private void setHealth(int health) {
-		this.health = health;
-	}
+    private void setHealth(int health) {
+        this.health = health;
+    }
 
-	public int getPoint() {
-		return point;
-	}
+    public int getPoint() {
+        return point;
+    }
 
-	private void setPoint(int point) {
-		this.point = point;
-	}
-	
-	public void incrementPoint(int increment) {
-		this.setPoint(this.getPoint()+increment);
-	}
-	
-	public void decrementHealth(int decrement) {
-		this.setHealth(this.getHealth()-decrement);
-	}
-	
-	
+    private void setPoint(int point) {
+        this.point = point;
+    }
+
+    public void incrementPoint(int increment) {
+        this.setPoint(this.getPoint() + increment);
+    }
+
+    public void decrementHealth(int decrement) {
+        this.setHealth(this.getHealth() - decrement);
+    }
+
+
 }
