6b28437eed5d5853a678280717ee927a6b66b59e
Aycan Deniz Vit <avit16@ku.edu.tr>
2020-12-27 19:07:14 +0300

    feat: add a simple template for local save load operations

--- src/main/java/dmme/kuvid/domain/database/SaveLoadLocal.java
+++ src/main/java/dmme/kuvid/domain/database/SaveLoadLocal.java
@@ -1,8 +1,29 @@
 package dmme.kuvid.domain.database;
 
+import dmme.kuvid.domain.GameObjects.GameObject;
+import dmme.kuvid.domain.GameObjects.Player;
+import dmme.kuvid.domain.KUVidGame;
+import dmme.kuvid.lib.types.Key;
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class SaveLoadLocal implements Saveable, Loadable {
+    private final HashMap<Key, List<GameObject>> gameObjectMap = KUVidGame.getGameObjectMap();
+    private ArrayList<GameObject> gameObjects;
+    private Player player;
+
+    public SaveLoadLocal() {
+    }
+
     @Override
     public boolean save() {
+        JSONArray jsonArray = new JSONArray();
+        for (Key key : gameObjectMap.keySet()) {
+            System.out.println(key.toString());
+        }
         return false;
     }
 
