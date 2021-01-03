4ba8964cdd60964b86df9014f56f11e95256b356
Aycan Deniz Vit <avit16@ku.edu.tr>
2021-01-03 15:09:39 +0300

    feat: add serialize and deserialize methods to local save class

--- src/main/java/dmme/kuvid/domain/database/SaveLoadFile.java
+++ src/main/java/dmme/kuvid/domain/database/SaveLoadFile.java
@@ -1,14 +1,30 @@
 package dmme.kuvid.domain.database;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import dmme.kuvid.domain.KUVidGame;
+
+
 public class SaveLoadFile implements SaveMode {
 
     @Override
     public boolean saveGame() {
+
         return false;
     }
 
     @Override
     public boolean loadGame() {
         return false;
+    }
+
+    private void serialize() {
+        Gson gson = new Gson();
+        String data = gson.toJson(KUVidGame.getGameObjectMap());
+    }
+
+    private void deserialize(Gson gson, String jsonString) {
+        //ype type = new TypeToken<HashMap<String, List<?>>>(){}.getType();
+        //HashMap<> clonedMap = gson.fromJson(jsonString, type);
     }
 }
