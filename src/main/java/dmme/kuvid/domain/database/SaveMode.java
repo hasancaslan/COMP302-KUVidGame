549a612aaae88be67750a0e1e0ed2a752f3ece01
Aycan Deniz Vit <avit16@ku.edu.tr>
2021-01-03 14:03:02 +0300

    fix: delete unnecessary fields

--- src/main/java/dmme/kuvid/domain/database/SaveMode.java
+++ src/main/java/dmme/kuvid/domain/database/SaveMode.java
@@ -1,9 +1,6 @@
 package dmme.kuvid.domain.database;
 
 public interface SaveMode {
-    boolean saveInfo = false;
-    boolean loadInfo = false;
-
     boolean saveGame();
     boolean loadGame();
 }
