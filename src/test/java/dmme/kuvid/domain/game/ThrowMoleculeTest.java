5556141594e9dbcb12cedbb5c86bd32511e6312b
Hasan Can <aslan4044@gmail.com>
2021-01-07 15:27:49 +0300

    test: add throw molecule list test

--- src/test/java/dmme/kuvid/domain/game/ThrowMoleculeTest.java
+++ src/test/java/dmme/kuvid/domain/game/ThrowMoleculeTest.java
@@ -1,15 +1,12 @@
 package dmme.kuvid.domain.game;
 
+import dmme.kuvid.domain.Controllers.DomainFactory;
 import dmme.kuvid.domain.Controllers.movementHandler;
-import dmme.kuvid.domain.GameObjects.Atoms.AlphaAtom;
 import dmme.kuvid.domain.GameObjects.GameObject;
-import dmme.kuvid.domain.GameObjects.Molecules.AlphaMolecule;
-import dmme.kuvid.domain.GameObjects.Position;
 import dmme.kuvid.domain.KUVidGame;
 import dmme.kuvid.lib.types.Key;
 import dmme.kuvid.lib.types.MoleculeType;
 import dmme.kuvid.lib.types.ObjectType;
-import dmme.kuvid.ui.BuildingWindow;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -17,61 +14,24 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ThrowMoleculeTest {
-    Position position = new Position(0, 0);
-    Position direction = new Position(10, 10);
-    MoleculeType moleculeType = MoleculeType.ALPHA;
-    ObjectType objectType = ObjectType.ATOM;
     GameObject molecule;
 
     @Test
     public void testList() {
-        BuildingWindow buildingWindow = new BuildingWindow();
-        buildingWindow.createGame();
-        List<GameObject> list = KUVidGame.getGameObjectMap().get(new Key(objectType, moleculeType));
+        KUVidGame.getInstance().setNumMolecules(10);
+        DomainFactory.createMolecule(MoleculeType.ALPHA, 10);
+        List<GameObject> list = KUVidGame.getGameObjectMap().get(new Key(ObjectType.MOLECULE, MoleculeType.ALPHA));
         assertNotNull(list, "Game Object list is null");
     }
 
     @Test
     public void checkMoleculeNum() {
+        KUVidGame.getInstance().setNumMolecules(10);
+        DomainFactory.createMolecule(MoleculeType.ALPHA, 10);
+        DomainFactory.createMolecule(MoleculeType.BETA, 10);
+        DomainFactory.createMolecule(MoleculeType.GAMMA, 10);
+        DomainFactory.createMolecule(MoleculeType.SIGMA, 10);
         movementHandler.getInstance().throwMolecule();
         assertTrue(molecule.getPosition().getX() == 0 && molecule.getPosition().getY() == 0);
     }
-
-    @Test
-    public void testPosition() {
-        molecule = new AlphaMolecule(position, direction, true, objectType);
-        assertTrue(molecule.getPosition().getX() == 0 && molecule.getPosition().getY() == 0);
-        molecule.setPosition(new Position(100, 100));
-        assertTrue(molecule.getPosition().getX() == 100 && molecule.getPosition().getY() == 100);
-    }
-
-    @Test
-    public void testActiveness() {
-        molecule = new AlphaAtom(position, direction, true, objectType);
-        assertTrue(molecule.isActive());
-        molecule.setActive(false);
-        assertFalse(molecule.isActive());
-    }
-
-    @Test
-    public void testMove() {
-        molecule = new AlphaMolecule(position, direction, true, objectType);
-        molecule.setPosition(new Position(0, 0));
-        molecule.setDirection(new Position(100, 100));
-        molecule.move();
-        assertTrue(molecule.getPosition().getX() == 100 && molecule.getPosition().getY() == 100);
-        molecule.move();
-        assertTrue(molecule.getPosition().getX() == 100 * (2) && molecule.getPosition().getY() == 100 * (2));
-        molecule.setDirection(new Position(-100, -100));
-        molecule.move();
-        assertTrue(molecule.getPosition().getX() == 100 && molecule.getPosition().getY() == 100);
-        molecule.move();
-        assertTrue(molecule.getPosition().getX() == 0 && molecule.getPosition().getY() == 0);
-        molecule.move();
-        assertNotEquals(molecule.getDirection().getX(), -100);
-        assertNotEquals(molecule.getPosition().getX(), -100);
-        assertEquals(100, molecule.getDirection().getX());
-        assertEquals(100, molecule.getPosition().getX());
-    }
-
 }
