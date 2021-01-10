fd7237ef390991482fa56b35564b01a662e33003
Hasan Can <aslan4044@gmail.com>
2021-01-10 21:09:40 +0300

    fix: resolve merge conflicts

--- src/test/java/dmme/kuvid/ThrowMoleculeTest.java
+++ src/test/java/dmme/kuvid/ThrowMoleculeTest.java
@@ -3,7 +3,6 @@ package dmme.kuvid;
 import dmme.kuvid.domain.Controllers.DomainFactory;
 import dmme.kuvid.domain.Controllers.movementHandler;
 import dmme.kuvid.domain.GameObjects.GameObject;
-import dmme.kuvid.domain.GameObjects.Molecules.Molecule;
 import dmme.kuvid.domain.KUVidGame;
 import dmme.kuvid.lib.types.Key;
 import dmme.kuvid.lib.types.MoleculeType;
@@ -41,7 +40,7 @@ public class ThrowMoleculeTest {
     @Test
     public void testList() {
         KUVidGame.getInstance().setNumMolecules(10);
-        DomainFactory.createMolecule(MoleculeType.ALPHA, 10);
+        DomainFactory.getInstance().createMolecule(MoleculeType.ALPHA, 10);
         List<GameObject> list = KUVidGame.getGameObjectMap().get(new Key(ObjectType.MOLECULE, MoleculeType.ALPHA));
         assertNotNull(list, "GameObject list is null.");
     }
@@ -49,10 +48,10 @@ public class ThrowMoleculeTest {
     @Test
     public void checkMoleculeNum() {
         KUVidGame.getInstance().setNumMolecules(4);
-        DomainFactory.createMolecule(MoleculeType.ALPHA, 1);
-        DomainFactory.createMolecule(MoleculeType.BETA, 1);
-        DomainFactory.createMolecule(MoleculeType.GAMMA, 1);
-        DomainFactory.createMolecule(MoleculeType.SIGMA, 1);
+        DomainFactory.getInstance().createMolecule(MoleculeType.ALPHA, 1);
+        DomainFactory.getInstance().createMolecule(MoleculeType.BETA, 1);
+        DomainFactory.getInstance().createMolecule(MoleculeType.GAMMA, 1);
+        DomainFactory.getInstance().createMolecule(MoleculeType.SIGMA, 1);
 
         List<GameObject> alphaList = KUVidGame.getGameObjectMap().get(new Key(ObjectType.MOLECULE, MoleculeType.ALPHA));
         List<GameObject> betaList = KUVidGame.getGameObjectMap().get(new Key(ObjectType.MOLECULE, MoleculeType.BETA));
